fe77221cff113f579977f7f46c216d85112f4d05
NeoNeonilla <nelliafleurova@gmail.com>
2021-07-21 17:29:16 -0500

    game plan map

---
+++ script.js
@@ -0,0 +1,11 @@
+//Create a timer that starts when the "start" button is clicked
+//create a CLICK event
+//inside CLICK event create a function If/else statement if wrong answer, TIME subtracted from total time
+//For questions create an Array[] that loops
+//Question Array is tied to the time function
+//once RIGHT or WRONG is determined it moves to the next array index
+//When one question assessed moves down the line of code
+//create an IF/OR END OF quiz function when all questions answerer OR timer is done
+//var endTime, var endQuiz, clearInterval. 
+//create function gameOver that contains imput for Initials and score, Local Storage?
+//Nice to have: restart
